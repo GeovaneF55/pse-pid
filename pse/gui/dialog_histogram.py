326848dd527004db9dd5cba1a64a3062b7e8de67
Gabriel Luciano <gabrielluciano26@gmail.com>
2018-10-16 14:27:11 -0300

    Histograma no Dialog + Icone reset preto

--- pse/gui/dialog_histogram.py
+++ pse/gui/dialog_histogram.py
@@ -2,6 +2,7 @@
 import cv2
 from matplotlib import pyplot as plt
 import numpy as np
+import os
 from PyQt5.QtCore import (Qt)
 from PyQt5.QtGui import (QPixmap)
 from PyQt5.QtWidgets import (QDialog,
@@ -52,13 +53,16 @@ class DialogHistogram(QDialog):
         #Adcionar o Histograma à Widget
         hLayout = QHBoxLayout()
         self.imgLabel = QLabel()
+        pixmap = QPixmap(HIST + 'hist.png')
+        self.imgLabel.setPixmap(pixmap)
+        self.imgLabel.show()
         hLayout.addWidget(self.imgLabel)
         layout.addRow(hLayout)
 
         # Butões de OK e Cancel
         buttons = QDialogButtonBox(
             QDialogButtonBox.Ok, Qt.Horizontal, self)
-        buttons.accepted.connect(self.accept)
+        buttons.accepted.connect(self.btnOkPressed)
         layout.addRow(buttons)
         
         self.setLayout(layout)
@@ -66,38 +70,38 @@ class DialogHistogram(QDialog):
 
 
     def btnState(self, b):
-        if b.text() == 'RBG':
+        if b.text() == 'RGB':
             if b.isChecked() == True:
-                pixmap = QPixmap(HIST + 'hist.png').scaled(256, 256)
+                pixmap = QPixmap(HIST + 'hist.png')
                 self.imgLabel.setPixmap(pixmap)
                 self.imgLabel.show()
                 
         if b.text() == 'RED':
             if b.isChecked() == True:
-                pixmap = QPixmap(HIST + 'red.png').scaled(256, 256)
+                pixmap = QPixmap(HIST + 'red.png')
                 self.imgLabel.setPixmap(pixmap)
                 self.imgLabel.show()
-                print(b.text() + ' is selected')
-            else:
-                print(b.text() + ' is deselected')
 
         if b.text() == 'GREEN':
             if b.isChecked() == True:
-                pixmap = QPixmap(HIST + 'green.png').scaled(256, 256)
+                pixmap = QPixmap(HIST + 'green.png')
                 self.imgLabel.setPixmap(pixmap)
                 self.imgLabel.show()
-                print(b.text() + ' is selected')
-            else:
-                print(b.text() + ' is deselected')
 
         if b.text() == 'BLUE':
             if b.isChecked() == True:
-                pixmap = QPixmap(HIST + 'blue.png').scaled(256, 256)
+                pixmap = QPixmap(HIST + 'blue.png')
                 self.imgLabel.setPixmap(pixmap)
                 self.imgLabel.show()
-                print(b.text() + ' is selected')
-            else:
-                print(b.text() + ' is deselected')
+
+    def btnOkPressed(self):
+        if os.path.exists(HIST + 'orig_tmp.jpg'):
+            os.remove(HIST + 'orig_tmp.jpg')
+            os.remove(HIST + 'hist.png')
+            os.remove(HIST + 'red.png')
+            os.remove(HIST + 'green.png')
+            os.remove(HIST + 'blue.png')
+        self.accept()
 
 
     def saveHistograms(self):
@@ -122,6 +126,7 @@ class DialogHistogram(QDialog):
         plt.plot(histr, color = 'r')
         plt.xlim([0,256])
         plt.savefig(HIST + 'red.png')
+        plt.clf()
 
 
     @staticmethod
