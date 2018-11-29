ee1d0526a891876984cd53de69ab43b989e96144
Gabriel Luciano <gabrielluciano26@gmail.com>
2018-11-28 22:24:01 -0200

    Dialog Histogramas corrigido

--- pse/gui/dialog_histogram.py
+++ pse/gui/dialog_histogram.py
@@ -1,16 +1,18 @@
 """ Bibliotecas externas. """
 import cv2
-from matplotlib import pyplot as plt
+from matplotlib         import pyplot as plt
 import numpy as np
 import os
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import (QDialog,
-                             QDialogButtonBox,
-                             QFormLayout,
-                             QHBoxLayout,
-                             QLabel,                             
-                             QRadioButton)
+from PyQt5.QtCore       import (Qt,
+                            QRect)
+from PyQt5.QtGui        import (QPixmap,
+                            QFont)
+from PyQt5.QtWidgets    import (QDialog,
+                            QDialogButtonBox,
+                            QFormLayout,
+                            QHBoxLayout,
+                            QLabel,                             
+                            QRadioButton)
 
 """ Biliotecas locais. """
 from util.resources import HIST
@@ -33,7 +35,7 @@ class DialogHistogram(QDialog):
         self.saveHistograms()      
         rButtons = QHBoxLayout()
         
-        self.b1 = QRadioButton('RGB')
+        self.b1 = QRadioButton('Escala de Cinza')
         self.b1.setChecked(True)
         self.b1.toggled.connect(lambda:self.btnState(self.b1))
         rButtons.addWidget(self.b1) 
@@ -50,16 +52,37 @@ class DialogHistogram(QDialog):
         self.b4.toggled.connect(lambda:self.btnState(self.b4))
         rButtons.addWidget(self.b4)    
 
+        rButtons.setAlignment(Qt.AlignCenter)
         layout = QFormLayout(self)
         layout.addRow(rButtons) 
 
-        #Adcionar o Histograma à Widget
+        l1 = QLabel()
+        l2 = QLabel()
+
+        #Adcionar o Histograma Img Original à Widget
+        l1.setText("Imagem Original")
+        layout.addRow(l1)
+        l1.setFont(QFont("Arial", 14, 75, False))
+        l1.setAlignment(Qt.AlignCenter)
         hLayout = QHBoxLayout()
-        self.imgLabel = QLabel()
-        pixmap = QPixmap(HIST + 'hist.png')
-        self.imgLabel.setPixmap(pixmap)
-        self.imgLabel.show()
-        hLayout.addWidget(self.imgLabel)
+        self.imgLabelOri = QLabel()
+        pixmap = QPixmap(HIST + 'hist_ori.png')
+        self.imgLabelOri.setPixmap(pixmap)
+        self.imgLabelOri.show()
+        hLayout.addWidget(self.imgLabelOri)
+        layout.addRow(hLayout)
+
+        #Adcionar o Histograma Img Processada à Widget
+        l2.setText("Última Imagem Processada")
+        layout.addRow(l2)
+        l2.setFont(QFont("Arial", 14, 75, False))
+        l2.setAlignment(Qt.AlignCenter)
+        hLayout = QHBoxLayout()
+        self.imgLabelProc = QLabel()
+        pixmap = QPixmap(HIST + 'hist_proc.png')
+        self.imgLabelProc.setPixmap(pixmap)
+        self.imgLabelProc.show()
+        hLayout.addWidget(self.imgLabelProc)
         layout.addRow(hLayout)
 
         # Butões de OK e Cancel
@@ -73,68 +96,113 @@ class DialogHistogram(QDialog):
 
 
     def btnState(self, b):
-        if b.text() == 'RGB':
+        if b.text() == 'Escala de Cinza':
             if b.isChecked() == True:
-                pixmap = QPixmap(HIST + 'hist.png')
-                self.imgLabel.setPixmap(pixmap)
-                self.imgLabel.show()
+                pixmap = QPixmap(HIST + 'hist_ori.png')
+                self.imgLabelOri.setPixmap(pixmap)
+                self.imgLabelOri.show()
+
+                pixmap = QPixmap(HIST + 'hist_proc.png')
+                self.imgLabelProc.setPixmap(pixmap)
+                self.imgLabelProc.show()
                 
         if b.text() == 'RED':
             if b.isChecked() == True:
-                pixmap = QPixmap(HIST + 'red.png')
-                self.imgLabel.setPixmap(pixmap)
-                self.imgLabel.show()
+                pixmap = QPixmap(HIST + 'red_ori.png')
+                self.imgLabelOri.setPixmap(pixmap)
+                self.imgLabelOri.show()
+
+                pixmap = QPixmap(HIST + 'red_proc.png')
+                self.imgLabelProc.setPixmap(pixmap)
+                self.imgLabelProc.show()
 
         if b.text() == 'GREEN':
             if b.isChecked() == True:
-                pixmap = QPixmap(HIST + 'green.png')
-                self.imgLabel.setPixmap(pixmap)
-                self.imgLabel.show()
+                pixmap = QPixmap(HIST + 'green_ori.png')
+                self.imgLabelOri.setPixmap(pixmap)
+                self.imgLabelOri.show()
+
+                pixmap = QPixmap(HIST + 'green_proc.png')
+                self.imgLabelProc.setPixmap(pixmap)
+                self.imgLabelProc.show()
 
         if b.text() == 'BLUE':
             if b.isChecked() == True:
-                pixmap = QPixmap(HIST + 'blue.png')
-                self.imgLabel.setPixmap(pixmap)
-                self.imgLabel.show()
+                pixmap = QPixmap(HIST + 'blue_ori.png')
+                self.imgLabelOri.setPixmap(pixmap)
+                self.imgLabelOri.show()
+
+                pixmap = QPixmap(HIST + 'blue_proc.png')
+                self.imgLabelProc.setPixmap(pixmap)
+                self.imgLabelProc.show()
+
 
     def btnOkPressed(self):
         if os.path.exists(HIST + 'orig_tmp.jpg'):
             os.remove(HIST + 'orig_tmp.jpg')
-            os.remove(HIST + 'hist.png')
-            os.remove(HIST + 'red.png')
-            os.remove(HIST + 'green.png')
-            os.remove(HIST + 'blue.png')
+            os.remove(HIST + 'hist_ori.png')
+            os.remove(HIST + 'red_ori.png')
+            os.remove(HIST + 'green_ori.png')
+            os.remove(HIST + 'blue_ori.png')
+            os.remove(HIST + 'proc_tmp.jpg')
+            os.remove(HIST + 'hist_proc.png')
+            os.remove(HIST + 'red_proc.png')
+            os.remove(HIST + 'green_proc.png')
+            os.remove(HIST + 'blue_proc.png')
         self.accept()
 
 
     def saveHistograms(self):
         img = cv2.imread(HIST + 'orig_tmp.jpg')
         plt.hist(img.ravel(), 256, [0,256])
-        plt.savefig(HIST + 'hist.png')
+        plt.savefig(HIST + 'hist_ori.png', dpi = 70)
         plt.clf()
 
         histr = cv2.calcHist([img],[0],None,[256],[0,256])
         plt.plot(histr, color = 'b')
         plt.xlim([0,256])
-        plt.savefig(HIST + 'blue.png')
+        plt.savefig(HIST + 'blue_ori.png', dpi = 70)
         plt.clf()
 
         histr = cv2.calcHist([img],[1],None,[256],[0,256])
         plt.plot(histr, color = 'g')
         plt.xlim([0,256])
-        plt.savefig(HIST + 'green.png')
+        plt.savefig(HIST + 'green_ori.png', dpi = 70)
         plt.clf()
 
         histr = cv2.calcHist([img],[2],None,[256],[0,256])
         plt.plot(histr, color = 'r')
         plt.xlim([0,256])
-        plt.savefig(HIST + 'red.png')
+        plt.savefig(HIST + 'red_ori.png', dpi = 70)
+        plt.clf()
+
+        img = cv2.imread(HIST + 'proc_tmp.jpg')
+        plt.hist(img.ravel(), 256, [0,256])
+        plt.savefig(HIST + 'hist_proc.png', dpi = 70)
+        plt.clf()
+
+        histr = cv2.calcHist([img],[0],None,[256],[0,256])
+        plt.plot(histr, color = 'b')
+        plt.xlim([0,256])
+        plt.savefig(HIST + 'blue_proc.png', dpi = 70)
+        plt.clf()
+
+        histr = cv2.calcHist([img],[1],None,[256],[0,256])
+        plt.plot(histr, color = 'g')
+        plt.xlim([0,256])
+        plt.savefig(HIST + 'green_proc.png', dpi = 70)
+        plt.clf()
+
+        histr = cv2.calcHist([img],[2],None,[256],[0,256])
+        plt.plot(histr, color = 'r')
+        plt.xlim([0,256])
+        plt.savefig(HIST + 'red_proc.png', dpi = 70)
         plt.clf()
 
 
     @staticmethod
-    def getResults(origImage, parent = None):
+    def getResults(origImage, procImage, parent = None):
         """ Método estático que cria o dialog e retorna true com sua finalização """
-        dialog = DialogHistogram(origImage, parent)
+        dialog = DialogHistogram(origImage, procImage, parent)
         result = dialog.exec_()
         return True
